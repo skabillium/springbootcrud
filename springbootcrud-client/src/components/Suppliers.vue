eafee7c428c7f6553d6f2acfce3968ba8c3d93c7
skabi <skabillium@gmail.com>
2019-06-15 17:32:41 +0300

    Suppliers template fully designed

--- springbootcrud-client/src/components/Suppliers.vue
+++ springbootcrud-client/src/components/Suppliers.vue
@@ -1,5 +1,57 @@
 <template>
-  <h1>Suppliers page</h1>
+  <div class="wrapper">
+    <div class="animated fadeIn">
+      <div class="row">
+        <div class="col-lg-12">
+          <el-card>
+            <div slot="header">
+              <p>Προμηθευτές</p>
+            </div>
+
+            <el-row>
+              <el-col :span="5">
+                <el-input v-model="query" placeholder="Search"></el-input>
+              </el-col>
+
+              <el-col :span="0.2">
+                <div class="grid-content bg-purple">&nbsp;</div>
+              </el-col>
+              <el-col :span="12">
+                <el-button class="el-button--primary" @click="refreshSuppliers()">Search</el-button>
+              </el-col>
+            </el-row>
+
+            <el-table :data="suppliers" stripe style="width: 100%">
+              <el-table-column
+                v-for="(field, index) in fields"
+                v-bind:key="index"
+                :prop="field.name"
+                :label="field.title"
+                :sortable="true"
+              ></el-table-column>
+              <el-table-column label="Operations" width="100">
+                <template slot-scope="scope">
+                  <el-button
+                    size="mini"
+                    class="el-button--warning"
+                    @click="onSupplierSelected(scope.row)"
+                  >Edit</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <div style="text-align: right; margin-top: 1em">
+              <el-button class="el-button--success" @click="createSupplier">
+                <i class="fa fa-plus"></i>
+                Δημιουργία
+              </el-button>
+            </div>
+          </el-card>
+        </div>
+      </div>
+    </div>
+    <Supplier></Supplier>
+  </div>
 </template>
 <script src="./SuppliersVM.js">
 </script>
